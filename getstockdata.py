2b6ee97edbcf2f7509481496d57854c4e0999846
deepak <sdeepak94@hotmail.com>
2017-11-10 15:04:17 -0700

    Stock Data

--- getstockdata.py
+++ getstockdata.py
@@ -1,9 +1,9 @@
 from pandas_datareader import data
 import datetime
 import csv
-startdate = datetime.datetime(2017,9,1)
-enddate = datetime.datetime(2017,10,1)
-stocks = ['AAPL','FB', 'GOOGL']
+startdate = datetime.datetime(2016,4,1)
+enddate = datetime.datetime(2016,6,15)
+stocks = ['AAPL','FB', 'GOOGL','MSFT']
 for stock in stocks:
 	f = data.DataReader(stock,'yahoo',startdate,enddate)
 	f.to_csv(stock+'.csv',mode='a')
