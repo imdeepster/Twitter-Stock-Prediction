74d5437d4f8e67f47d8c508183ee8ae5caf7320c
deepak <sdeepak94@hotmail.com>
2017-11-12 22:03:39 -0700

    Amazon Stock data

--- getstockdata.py
+++ getstockdata.py
@@ -1,15 +1,9 @@
 from pandas_datareader import data
 import datetime
 import csv
-<<<<<<< HEAD
-startdate = datetime.datetime(2016,4,2)
+startdate = datetime.datetime(2016,3,29)
 enddate = datetime.datetime(2016,6,15)
-stocks = ['AAPL','FB', 'GOOGL']
-=======
-startdate = datetime.datetime(2016,4,1)
-enddate = datetime.datetime(2016,6,15)
-stocks = ['AAPL','FB', 'GOOGL','MSFT']
->>>>>>> 2b6ee97edbcf2f7509481496d57854c4e0999846
+stocks = ['AMZN']
 for stock in stocks:
 	f = data.DataReader(stock,'yahoo',startdate,enddate)
 	f.to_csv(stock+'.csv',mode='a')
